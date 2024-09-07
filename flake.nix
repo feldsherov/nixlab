867822d00438d5f5b9ec03cb85b2bcd710ac5835
Svyatoslav Feldsherov <svyat@feldsherov.name>
2024-09-07 11:29:43 +0300

    add hosts directory

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
+      newt = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
