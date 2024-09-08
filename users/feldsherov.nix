71f8a08b9686aa1c3e14c6fb5d496d85bf2c12a8
Svyatoslav Feldsherov <svyat@feldsherov.name>
2024-09-08 20:32:33 +0300

    use full latex

--- users/feldsherov.nix
+++ users/feldsherov.nix
@@ -2,7 +2,7 @@
 
 let
   tex = (pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-medium
+    inherit (pkgs.texlive) scheme-full
       dvisvgm dvipng # for preview and export as html
       wrapfig amsmath ulem hyperref capt-of;
       #(setq org-latex-compiler "lualatex")
