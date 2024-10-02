64902cc38fb842c1f643bce4f1109b6bfab7e8ea
Svyatoslav Feldsherov <svyat@feldsherov.name>
2024-10-02 13:12:39 +0300

    polish sway configuration

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -1,16 +1,153 @@
 { config, pkgs, ... }:
 {
   wayland.windowManager.sway = {
-    enable = true;
-    config = {
-      modifier = "Mod4";
-      terminal = "foot"; 
-      input = {
-          "type:keyboard" = {
-            xkb_layout = "us,ru";
-            xkb_options = "grp:win_space_toggle,caps:swapescape";
+      enable = true;
+      config = rec {
+        modifier = "Mod4";
+        terminal = "foot"; 
+	menu = "${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu | ${pkgs.findutils}/bin/xargs swaymsg exec --";
+        input = {
+           "type:keyboard" = {
+             xkb_layout = "us,ru";
+             xkb_options = "grp:win_space_toggle,caps:swapescape";
+           };
+           "type:touchpad" = {
+  	   natural_scroll = "enabled";
+           };
+        };
+        left = "h";
+	right = "l";
+        down = "j";
+        up = "k";
+        keybindings = {
+          "${modifier}+Return" = "exec ${terminal}";
+          "${modifier}+Shift+q" = "kill";
+          "${modifier}+d" = "exec ${menu}";
+
+          "${modifier}+${left}" = "focus left";
+          "${modifier}+${down}" = "focus down";
+          "${modifier}+${up}" = "focus up";
+          "${modifier}+${right}" = "focus right";
+
+          "${modifier}+Left" = "focus left";
+          "${modifier}+Down" = "focus down";
+          "${modifier}+Up" = "focus up";
+          "${modifier}+Right" = "focus right";
+
+          "${modifier}+Shift+${left}" = "move left";
+          "${modifier}+Shift+${down}" = "move down";
+          "${modifier}+Shift+${up}" = "move up";
+          "${modifier}+Shift+${right}" = "move right";
+
+          "${modifier}+Shift+Left" = "move left";
+          "${modifier}+Shift+Down" = "move down";
+          "${modifier}+Shift+Up" = "move up";
+          "${modifier}+Shift+Right" = "move right";
+
+          "${modifier}+b" = "splith";
+          "${modifier}+v" = "splitv";
+          "${modifier}+f" = "fullscreen toggle";
+          "${modifier}+a" = "focus parent";
+
+          "${modifier}+s" = "layout stacking";
+          "${modifier}+w" = "layout tabbed";
+          "${modifier}+e" = "layout toggle split";
+
+          "${modifier}+Shift+space" = "floating toggle";
+          "${modifier}+space" = "focus mode_toggle";
+
+          "${modifier}+1" = "workspace number 1";
+          "${modifier}+2" = "workspace number 2";
+          "${modifier}+3" = "workspace number 3";
+          "${modifier}+4" = "workspace number 4";
+          "${modifier}+5" = "workspace number 5";
+          "${modifier}+6" = "workspace number 6";
+          "${modifier}+7" = "workspace number 7";
+          "${modifier}+8" = "workspace number 8";
+          "${modifier}+9" = "workspace number 9";
+          "${modifier}+0" = "workspace number 10";
+
+          "${modifier}+Shift+1" =
+            "move container to workspace number 1";
+          "${modifier}+Shift+2" =
+            "move container to workspace number 2";
+          "${modifier}+Shift+3" =
+            "move container to workspace number 3";
+          "${modifier}+Shift+4" =
+            "move container to workspace number 4";
+          "${modifier}+Shift+5" =
+            "move container to workspace number 5";
+          "${modifier}+Shift+6" =
+            "move container to workspace number 6";
+          "${modifier}+Shift+7" =
+            "move container to workspace number 7";
+          "${modifier}+Shift+8" =
+            "move container to workspace number 8";
+          "${modifier}+Shift+9" =
+            "move container to workspace number 9";
+          "${modifier}+Shift+0" =
+            "move container to workspace number 10";
+
+          "${modifier}+Shift+minus" = "move scratchpad";
+          "${modifier}+minus" = "scratchpad show";
+
+          "${modifier}+Shift+c" = "reload";
+          "${modifier}+Shift+e" =
+            "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+
+          "${modifier}+r" = "mode resize";
+          "${modifier}+z" = "exec swaylock " +
+             " --screenshots --clock --indicator" +
+             " --indicator-radius 100 --indicator-thickness 7" +
+             " --effect-blur 7x5 --effect-vignette 0.5:0.5" +
+             " --ring-color 00ff88 --key-hl-color 880033" +
+             " --line-color 00000000 --inside-color 00000088" +
+             " --separator-color 00000000 --grace 2 --fade-in 0.2";
+        }; 
+      
+      bars = [{
+        mode = "dock";
+        hiddenState = "hide";
+        position = "top";
+        workspaceButtons = true;
+        workspaceNumbers = true;
+        statusCommand = "${pkgs.i3status}/bin/i3status";
+        fonts = {
+          names = [ "monospace" ];
+          size = 8.0;
+        };
+        trayOutput = "primary";
+        colors = {
+          background = "#000000";
+          statusline = "#ffffff";
+          separator = "#666666";
+          focusedWorkspace = {
+            border = "#4c7899";
+            background = "#285577";
+            text = "#ffffff";
           };
-      };
+          activeWorkspace = {
+            border = "#333333";
+            background = "#5f676a";
+            text = "#ffffff";
+          };
+          inactiveWorkspace = {
+            border = "#333333";
+            background = "#222222";
+            text = "#888888";
+          };
+          urgentWorkspace = {
+            border = "#2f343a";
+            background = "#900000";
+            text = "#ffffff";
+          };
+          bindingMode = {
+            border = "#2f343a";
+            background = "#900000";
+            text = "#ffffff";
+            };
+        };
+      }];
     };
   };
 
@@ -20,5 +157,6 @@
     grim
     slurp
     wl-clipboard
+    foot
   ];
 }
