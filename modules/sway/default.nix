741b1d29b024ed54f8615972103786236cdc3953
Svyatoslav Feldsherov <svyat@feldsherov.name>
2024-10-02 13:04:09 +0300

    polish sway configuration

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -2,6 +2,16 @@
 {
   wayland.windowManager.sway = {
     enable = true;
+    config = {
+      modifier = "Mod4";
+      terminal = "foot"; 
+      input = {
+          "type:keyboard" = {
+            xkb_layout = "us,ru";
+            xkb_options = "grp:win_space_toggle,caps:swapescape";
+          };
+      };
+    };
   };
 
   home.packages = with pkgs; [
