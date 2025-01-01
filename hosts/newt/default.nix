f2d23a076b3bd8c36535dd4e7fdf924f0a17f2c3
Svyatoslav Feldsherov <svyat@feldsherov.name>
2025-01-01 17:57:43 +0200

    dump kernel cores

--- hosts/newt/default.nix
+++ hosts/newt/default.nix
@@ -14,6 +14,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Dump kernel cores (hopefully)
+  boot.crashDump.enable = true;
+
+
   networking.hostName = "newt"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
