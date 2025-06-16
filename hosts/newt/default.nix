a21035f9bd93d5141bbb8ee2dffed577ebff5f87
Svyatoslav Feldsherov <svyat@feldsherov.name>
2025-06-16 19:16:03 +0300

    install wireshark and steam on newt

--- hosts/newt/default.nix
+++ hosts/newt/default.nix
@@ -70,11 +70,19 @@
     bison
     pavucontrol
     docker
+    wireshark
   ];
 
   # Set vim as default editor.
   programs.vim.enable = true;
   programs.vim.defaultEditor = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+
 
   security.rtkit.enable = true;
   services.pipewire = {
